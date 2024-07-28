0c13b290fee11fbb82f51c3a3343690dabd88039
imam-masum <imammasum.biu@gmail.com>
2024-07-28 11:57:13 -0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿//Load sample data
+using MlApp;
+
+var sampleData = new Santimentmodel.ModelInput()
+{
+    Col0 = @"Crust is not good.",
+};
+
+//Load model and predict output
+var result = Santimentmodel.Predict(sampleData);
+var sentiment=result.PredictedLabel==1? "Positive " : " Negative";
+Console.WriteLine($"Text{sampleData.Col0}\n Sentiment{sentiment}");
